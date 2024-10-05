e9427dc31be7294838ac8afca1957b6cee693061
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-05 11:00:07 +0300

    use endpoints in the server that are authorization and authentication related

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const authRoutes = require("./routes/auth-routes");
 
 const app = express();
 
 const PORT = process.env.PORT;
 
 app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port number ${PORT}...`);
