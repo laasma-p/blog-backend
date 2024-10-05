3f3fdef8a625ac24df6caedbca3f68f251c82be6
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-05 11:05:30 +0300

    create initial User model

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../sequelize");
+
+const User = sequelize.define(
+  "user",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = User;
