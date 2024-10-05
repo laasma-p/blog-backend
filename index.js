cb3658aa552486d3c2d7fddd7f0fc153b2103cc2
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-05 11:22:27 +0300

    use endpoints related to posts in the server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const authRoutes = require("./routes/auth-routes");
+const postsRoutes = require("./routes/posts-routes");
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/posts", postsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port number ${PORT}...`);
