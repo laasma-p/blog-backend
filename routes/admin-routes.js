ae3aed9b16bd01b6a819ed7bbe38ad9a8a58e62b
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-07 10:41:27 +0300

    add fetching list of posts with relevant attributes endpoint

---
+++ routes/admin-routes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/post");
+const authenticateToken = require("../middleware/jwt");
+
+router.get("/posts-list", authenticateToken, async (req, res) => {
+  try {
+    const posts = await Post.findAll({
+      where: { userId: req.userId },
+      attributes: ["title", "date", "isPinned"],
+    });
+
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+module.exports = router;
