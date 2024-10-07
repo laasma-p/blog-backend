a5656bb054ecbb17c6597a51e95230da002fb86a
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-07 09:57:05 +0300

    create JWT authorization token validation function

---
+++ middleware/jwt.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+const authenticateToken = (req, res, next) => {
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized." });
+  }
+
+  jwt.verify(token.split(" ")[1], process.env.JWT_SECRET, (error, user) => {
+    if (error) {
+      return res.status(403).json({ message: "Access denied." });
+    }
+
+    req.userId = user.userId;
+    next();
+  });
+};
+
+module.exports = authenticateToken;
