dd4f5720f7a180f960146fb90a4b1c2e4b0d6107
Lasma Poksane <laasmapoksane@gmail.com>
2024-11-02 02:29:18 +0100

    create timestamps for a table and remove date attribute

--- models/post.js
+++ models/post.js
@@ -2,41 +2,30 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../sequelize");
 const User = require("./user");
 
-const Post = sequelize.define(
-  "post",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    status: {
-      type: DataTypes.ENUM("published", "draft"),
-      allowNull: false,
-      defaultValue: "published",
-    },
-    date: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    isPinned: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
+const Post = sequelize.define("post", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    timestamps: false,
-  }
-);
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.ENUM("published", "draft"),
+    allowNull: false,
+    defaultValue: "published",
+  },
+  isPinned: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
 
 Post.belongsTo(User, { foreignKey: "userId", onDelete: "CASCADE" });
 User.hasMany(Post, { foreignKey: "userId" });
